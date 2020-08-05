ec78c843fb5f9cce44fcf2a4bd39009b3cfb0032
dev09 <devdevdevdevdev09@gmail.com>
2020-08-05 14:09:07 +0900

    init project

---
+++ src/test/java/com/heo/slacktohtml/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heo.slacktohtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
