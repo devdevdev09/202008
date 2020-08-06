142d73c4ceea6444d79742b193ff30949288db49
dev09 <devdevdevdevdev09@gmail.com>
2020-08-06 14:23:24 +0900

    zip 파일 업로드 후 내용 리스트에 출력

--- src/main/java/com/heo/slacktohtml/controller/FileUploadController.java
+++ src/main/java/com/heo/slacktohtml/controller/FileUploadController.java
@@ -1,20 +1,13 @@
 package com.heo.slacktohtml.controller;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
 import java.util.List;
-import java.util.UUID;
-import java.util.zip.ZipEntry;
+import java.util.Map;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
 
 import com.heo.slacktohtml.service.FileUploadService;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RequestMapping("/fileupload")
 @Controller
@@ -53,57 +47,27 @@ public class FileUploadController {
     String FILE_UPLOAD_PATH;
 
     @PostMapping("/uploadzip")
-    public String zipFileUpload(@RequestParam("file") MultipartFile mfile) {
+    public String zipFileUpload(@RequestParam("file") MultipartFile mfile
+                                ,Model model) {
         try {
-            // save file to temp
-            // File zip = File.createTempFile(UUID.randomUUID().toString(), "temp");
-            // FileOutputStream o = new FileOutputStream(zip);
-            // IOUtils.copy(file.getInputStream(), o);
-            // o.close();
-
-            // unzip file from temp by zip4j
-            String destination = "D:\\destination";
-            //ZipFile zipFile = new ZipFile(zip);
-
-            File file = new File(FILE_UPLOAD_PATH + mfile.getOriginalFilename());
-            mfile.transferTo(file);
-
-
-            ZipFile zipFile = new ZipFile(file);
-            Enumeration<? extends ZipEntry> list = zipFile.entries();
+            File file = fileUploadService.multipartToFile(mfile);
             
+            ZipFile zipFile = new ZipFile(file);
 
-            while(list.hasMoreElements()){
-                ZipEntry zip2 = list.nextElement();
+            List<Map<String, Object>> list = fileUploadService.readContent(zipFile);
 
-                if(zip2.isDirectory()){
-                    System.out.println(zip2.getName());
-                }else{
-                    System.out.println("\t[파일]" + zip2.getName());
-                }
-                
+            zipFile.close();
 
-                if(!zip2.isDirectory()){
-                    InputStream stream = zipFile.getInputStream(zip2);
-                
-                    String str = fileUploadService.byteToString(stream.readAllBytes());
-
-                    // System.out.println("\t\t content :: " + str);
-
-                    stream.close();
-                }
+            if(file.exists()){
+                boolean isDelete = file.delete();
+                logger.debug("파일 삭제 " + ((isDelete)? "성공" : "실패"));
             }
 
-            if(file.delete()){
-                System.out.println("파일 삭제 성공");
-            }else{
-                System.out.println("파일 삭제 실패");
-            }
-
+            model.addAttribute("result", list);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "index";
+        return "result/result1";
     }
     
 }
--- src/main/java/com/heo/slacktohtml/service/FileUploadService.java
+++ src/main/java/com/heo/slacktohtml/service/FileUploadService.java
@@ -1,8 +1,19 @@
 package com.heo.slacktohtml.service;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Service;
@@ -10,10 +21,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
-    
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public String fileToString(MultipartFile file) {
         String string = "";
-        
+
         try {
             string = new String(file.getBytes());
         } catch (IOException e) {
@@ -23,17 +35,80 @@ public class FileUploadService {
         return string;
     }
 
-    public String byteToString(byte[] byts){
+    public String byteToString(byte[] byts) {
         String string = new String(byts);
 
         return string;
     }
-    
-    public List<Object> stringToList(String string){
+
+    public List<Object> stringToList(String string) {
         List<Object> list;
 
         JsonParser jp = JsonParserFactory.getJsonParser();
-        list = jp.parseList(string);        
+        list = jp.parseList(string);
+
+        return list;
+    }
+
+    @Value("${FILE_UPLOAD_PATH}")
+    String FILE_UPLOAD_PATH;
+
+    public File multipartToFile(MultipartFile mfile) {
+        File file = new File(FILE_UPLOAD_PATH + mfile.getOriginalFilename());
+        
+        try {
+            mfile.transferTo(file);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return file;
+    }
+
+    public List<Map<String,Object>> readContent(ZipFile zipFile){
+        List<Map<String,Object>> list = new ArrayList<Map<String, Object>>();
+
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        
+        while(entries.hasMoreElements()){
+            Map<String, Object> item = new HashMap<String, Object>();
+            ZipEntry zipEntry = entries.nextElement();
+
+            String fileName = zipEntry.getName();
+            String[] arr = fileName.split("/");
+
+
+            int idx = fileName.lastIndexOf("/");
+
+            if(!zipEntry.isDirectory()){
+                item.put("isDirectory", false);
+
+                
+                // String channel = fileName.substring(0, idx);
+                // String date = fileName.substring(idx + 1);
+                // logger.debug("channel : date :: " + channel + " : " + date);
+
+                String content;
+                try {
+                    InputStream inStream = zipFile.getInputStream(zipEntry);
+                    content = byteToString(inStream.readAllBytes());
+
+                    item.put("content", content);
+                    logger.debug("fileName :: " + content);
+
+                    inStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                item.put("isDirectory", true);    
+            }
+            item.put("name", fileName);
+
+            list.add(item);
+        }
 
         return list;
     }
